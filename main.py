be59e39caa8dfb4c1ae78f704fcdcdf3922ed209
IlaAmini <s220069@dtu.dk>
2025-05-13 14:44:00 +0200

    added  ner_extractor + new conda env with spacy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from parser_utils.file_reader import FileReader
 from parser_utils.text_cleaner import TextCleaner
+from parser_utils.ner_extractor import NERExtractor
 
 
 file_path = "data/LinkedInCV.pdf"  # Change to .docx or .txt if needed
@@ -10,10 +11,16 @@ try:
     reader = FileReader(file_path)
     raw_text = reader.read()
 
+    # clean resume
     cleaner = TextCleaner(raw_text)
     text = cleaner.clean()
+
+    # Extract entities
+    extractor = NERExtractor()
+    entities = extractor.extract_entities(text)
     
-    print("cleaned Resume Text:\n")
-    print(text)  
+    print("\n🎯 Extracted Entities:")
+    for key, value in entities.items():
+        print(f"{key.title()}: {value}")
 except Exception as e:
     print(f"Error reading file: {e}")
